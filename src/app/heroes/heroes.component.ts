17cf5767a08e33f7df1cf12be7cf289edfd86a5f
simplecov <1kbspeed@gmail.com>
2018-08-23 18:23:19 +0300

    Эдитор, вывод списка, начало работы с hero-detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +10,21 @@ import { Hero } from '../hero';
 
 export class HeroesComponent implements OnInit {
 
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    heroes = HEROES;
+    selectedHero: Hero;
+    // hero: Hero = {
+    //     id: 1,
+    //     name: 'Windstorm'
+    // };
 
-  constructor() { }
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 
-  ngOnInit() {
-    console.log('hero component');
-  }
+    constructor() { }
+
+    ngOnInit() {
+
+    }
 
 }
